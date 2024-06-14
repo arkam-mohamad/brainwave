2c650f8ef5d635549effbf0010da463943737883
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2024-06-14 08:44:07 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,121 @@
+import brainwave from "./brainwave.svg";
+import check from "./check.svg";
+import brainwaveSymbol from "./brainwave-symbol.svg";
+import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
+import play from "./play.svg";
+import gradient from "./gradient.png";
+import smallSphere from "./4-small.png";
+import grid from "./grid.png";
+import check2 from "./check-02.svg";
+import loading1 from "./loading-01.svg";
+import yourlogo from "./yourlogo.svg";
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import recording03 from "./recording-03.svg";
+import recording01 from "./recording-01.svg";
+import disc02 from "./disc-02.svg";
+import chromecast from "./chrome-cast.svg";
+import sliders04 from "./sliders-04.svg";
+import loading from "./loading.png";
+import background from "./background.jpg";
+
+import curve from "./hero/curve.svg";
+import robot from "./hero/robot.jpg";
+import heroBackground from "./hero/hero-background.jpg";
+
+import curve1 from "./collaboration/curve-1.svg";
+import curve2 from "./collaboration/curve-2.svg";
+import discord from "./collaboration/discord.svg";
+import figma from "./collaboration/figma.svg";
+import framer from "./collaboration/framer.svg";
+import notion from "./collaboration/notion.svg";
+import photoshop from "./collaboration/photoshop.svg";
+import protopie from "./collaboration/protopie.svg";
+import raindrop from "./collaboration/raindrop.svg";
+import slack from "./collaboration/slack.svg";
+
+import service1 from "./services/service-1.png";
+import service2 from "./services/service-2.png";
+import service3 from "./services/service-3.png";
+
+import lines from "./pricing/lines.svg";
+import stars from "./pricing/stars.svg";
+
+import benefitCard1 from "./benefits/card-1.svg";
+import benefitCard2 from "./benefits/card-2.svg";
+import benefitCard3 from "./benefits/card-3.svg";
+import benefitCard4 from "./benefits/card-4.svg";
+import benefitCard5 from "./benefits/card-5.svg";
+import benefitCard6 from "./benefits/card-6.svg";
+import benefitIcon1 from "./benefits/icon-1.svg";
+import benefitIcon2 from "./benefits/icon-2.svg";
+import benefitIcon3 from "./benefits/icon-3.svg";
+import benefitIcon4 from "./benefits/icon-4.svg";
+import benefitImage2 from "./benefits/image-2.png";
+
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+
+export {
+  brainwave,
+  check,
+  check2,
+  loading1,
+  brainwaveSymbol,
+  brainwaveWhiteSymbol,
+  play,
+  gradient,
+  smallSphere,
+  grid,
+  yourlogo,
+  homeSmile,
+  file02,
+  searchMd,
+  plusSquare,
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+  loading,
+  background,
+  curve,
+  robot,
+  heroBackground,
+  curve1,
+  curve2,
+  discord,
+  figma,
+  framer,
+  notion,
+  photoshop,
+  protopie,
+  raindrop,
+  slack,
+  service1,
+  service2,
+  service3,
+  lines,
+  stars,
+  benefitCard1,
+  benefitCard2,
+  benefitCard3,
+  benefitCard4,
+  benefitCard5,
+  benefitCard6,
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+};
